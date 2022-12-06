ad3cf95f6f3900c1a86ec8c14351a15c7493186f
robert-moonshot <rob.moonshot@gmail.com>
2022-12-06 17:47:19 +0800

    issue/navbar

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -21,34 +21,28 @@ export default function NavigationPanel({
   return (
     <NavigationBar
       rightElements={
-        <div>
-          {authenticated ? (
-            <UserAvatar />
-          ) : (
-            <div className="flex space-x-6 items-center justify-start">
-              <Search size="sm" className="lg:visible invisible" />
-              <p
-                className="text-sm font-medium leading-tight text-blue-700 w-24 cursor-pointer"
-                onClick={() => router.push("login")}
-              >
-                Sign in
-              </p>
+        <div className="flex sm:space-x-6 items-center justify-end">
+          <Search size="sm" className="lg:block hidden" />
+          <p
+            className="text-sm font-medium leading-tight text-blue-700 w-24 cursor-pointer"
+            onClick={() => router.push("/login")}
+          >
+            Sign in
+          </p>
 
-              <div className="relative" style={{ width: "100%", height: 40 }}>
-                <div
-                  className="inline-flex items-center justify-center px-5 py-2 bg-blue-700 rounded-lg"
-                  style={{ width: "100%", height: 40 }}
-                >
-                  <p
-                    className="text-xs font-semibold leading-snug text-center text-gray-50"
-                    onClick={() => setAuthenticated(true)}
-                  >
-                    Try it free
-                  </p>
-                </div>
-              </div>
+          <div
+            className="sm:inline-flex hidden relative"
+            style={{ width: "100%", height: 40 }}
+          >
+            <div
+              className="inline-flex items-center justify-center px-5 py-2 bg-blue-700 rounded-lg"
+              style={{ width: "100%", height: 40 }}
+            >
+              <p className="text-xs font-semibold leading-snug text-center text-gray-50">
+                Try it free
+              </p>
             </div>
-          )}
+          </div>
         </div>
       }
       centerElements={
