e0b25b443cb6da64818fea6a661a363472694db5
robert-moonshot <rob.moonshot@gmail.com>
2022-12-14 17:11:04 +0800

    recommit

--- services/catalog.tsx
+++ services/catalog.tsx
@@ -1,22 +1,19 @@
 export const getCatalog = async () => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/Catalog`,
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-    }
-  );
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/Catalog`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
   const content = await response.json();
   return content;
 };
 export const getCatalogById = async (id: string) => {
   console.log("data");
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/Catalog/${id}`,
+    `${process.env.NEXT_PUBLIC_API_URL}/Catalog/${id}`,
     {
       method: "GET",
       headers: {
